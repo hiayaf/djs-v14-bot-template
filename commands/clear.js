c90cd499f4eb7494ecefa5d59af9114b5db08068
hiayaf <98193137+hiayaf@users.noreply.github.com>
2022-09-11 01:54:45 +0200

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('clear')
+        .setDescription('Czyści chat')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages | PermissionFlagsBits.ModerateMembers)
+        .addIntegerOption(option =>
+            option.setName('ilość')
+                .setDescription('Ilość wiadomości do usunięcia (wartość od 1 do 100)')
+                .setRequired(true)),
+    async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true });
+        const count = interaction.options.getInteger('ilość');
+        if (count < 101 && count > 0) {
+            await interaction.channel.bulkDelete(count, true).then(msg => { interaction.followUp({ content: `**Pomyślnie usunięto** \`${msg.size}\` **wiadomości.**`, ephemeral: true }); })
+        } else {
+            await interaction.followUp({ content: `**Nieprawidłowa wartość...** \`${count}\``, ephemeral: true });
+        };
+    }
+};
