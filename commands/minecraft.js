136bb662dac0fe77afd6989585bdd920ad850f1c
hiayaf <98193137+hiayaf@users.noreply.github.com>
2022-09-30 14:17:54 +0200

    Update minecraft.js

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -3,43 +3,33 @@ const request = require('request');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('server-status')
-        .setNameLocalizations({
-            pl: 'status-serwera',
-        })
+        .setNameLocalizations({ pl: 'status-serwera' })
         .setDescription('Shows the current status of the minecraft server')
-        .setDescriptionLocalizations({
-            pl: 'Pokazuje aktualny status serwera minecraft',
-        })
+        .setDescriptionLocalizations({ pl: 'Pokazuje aktualny status serwera minecraft' })
         .addStringOption(option =>
             option.setName('address')
-                .setNameLocalizations({
-                    pl: 'adres',
-                })
+                .setNameLocalizations({ pl: 'adres' })
                 .setDescription('Enter the address of the minecraft server')
-                .setDescriptionLocalizations({
-                    pl: 'Wpisz adres serwera Minecraft',
-                })
+                .setDescriptionLocalizations({ pl: 'Wpisz adres serwera Minecraft' })
                 .setRequired(true)),
     async execute(interaction) {
-        var request = require('request');
         const server = interaction.options.getString('address');
         if (/^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/.test(server)) {
             var url = 'https://api.mcsrvstat.us/2/' + server;
             request(url, function (err, response, body) {
                 try {
                     body = JSON.parse(body);
-                    if (!err) {
-                        var status = '**Wyłączony**';
-                        if (body.online) {
-                            status = '**Włączony**';
-                        };
-                        const embed = new EmbedBuilder()
-                            .setThumbnail(`https://mc-api.net/v3/server/favicon/${server}`)
-                            .setTitle(`Status serwera ${server}`)
-                            .setDescription(`${status}\n${body.players.online}/${body.players.max}\nWersja: ${body.version}`)
-                            .setColor('DARK_BLUE');
-                        interaction.reply({ embeds: [embed], ephemeral: false });
-                    }
+                    if (err) return;
+                    var status = '**Wyłączony**';
+                    if (body.online) {
+                        status = '**Włączony**';
+                    };
+                    const embed = new EmbedBuilder()
+                        .setThumbnail(`https://mc-api.net/v3/server/favicon/${server}`)
+                        .setTitle(`Status serwera ${server}`)
+                        .setDescription(`${status}\n${body.players.online}/${body.players.max}\nWersja: ${body.version}`)
+                        .setColor('DARK_BLUE');
+                    interaction.reply({ embeds: [embed], ephemeral: false });
                 } catch (err) {
                     if (interaction.deferred || interaction.replied) return;
                     interaction.reply({ content: `**Wystąpił błąd podczas uzyskiwania statusu serwera ${server}**`, ephemeral: true });
