92ff36e01ec4c687af01df1f8471bce5b87e1433
hiayaf <98193137+hiayaf@users.noreply.github.com>
2022-10-02 22:29:48 +0200

    Update apod_nasa.js

--- commands/apod_nasa.js
+++ commands/apod_nasa.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const request = require('request');
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,13 +6,13 @@ module.exports = {
         .setDescription('Nasa'),
     async execute(interaction) {
         await interaction.deferReply();
-        let url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+        let url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
         request(url, function (err, response, body) {
             try {
                 body = JSON.parse(body);
             } catch (error) {
-                return interaction.reply({ content: `Wystąpił błąd podczas uzyskiwania informacji od NASA`, ephemeral: true })
-            }
+                return interaction.reply({ content: `Wystąpił błąd podczas uzyskiwania informacji od NASA`, ephemeral: true });
+            };
             if (!err) {
                 const embed = new EmbedBuilder()
                     .setImage(body.url)
@@ -22,9 +22,9 @@ module.exports = {
                         { name: '\u200B', value: `**${body.explanation}**` }
                     )
                     .setFooter({ text: body.date })
-                    .setAuthor({ name: body.copyright })
-                interaction.editReply({ embeds: [embed], ephemeral: false })
-            }
-        })
+                    .setAuthor({ name: body.copyright });
+                interaction.editReply({ embeds: [embed], ephemeral: false });
+            };
+        });
     }
 };	
