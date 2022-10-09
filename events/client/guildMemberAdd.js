1314d781845e961a40b766a18aa9112b96f77386
hiayaf <98193137+hiayaf@users.noreply.github.com>
2022-10-09 15:25:27 +0200

    Update guildMemberAdd.js

--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -1,12 +1,11 @@
 const config = require('../../config.json');
 module.exports = async (client, member) => {
+    //auto_role
+    let math = Math.floor(Date.now() / 1000) - config.auto_role.account_time
+    if (math >= config.auto_role.account_time) {
+        member.roles.add(config.auto_role.role);
+    }
     const channel = client.channels.cache.get(config.channels.join);
     if (!channel) return console.log('Nie znaleziono kanału powitań');
     channel.send(`**Witaj ${member} na serwerze!**`);
-
-    //auto_role
-
-    const user = await client.users.fetch(member.id);
-    let timestamp = Math.floor(Date.now() / 1000);
-    if (timestamp - user.createdTimestamp <= config.auto_role.account_time) return member.roles.add(config.auto_role.role);
 };
