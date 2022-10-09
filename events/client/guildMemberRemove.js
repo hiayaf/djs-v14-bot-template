9f067ee24cc0e5a00dbccdfdaa1502b80afcfda4
hiayaf <98193137+hiayaf@users.noreply.github.com>
2022-10-09 15:26:07 +0200

    Update guildMemberRemove.js

--- events/client/guildMemberRemove.js
+++ events/client/guildMemberRemove.js
@@ -1,6 +1,6 @@
 const config = require('../../config.json');
 module.exports = async (client, member) => {
     const channel = client.channels.cache.get(config.channels.leave);
-    if (!channel) return console.log('Nie znaleziono leave channel');
+    if (!channel) return console.log('Nie znaleziono kanału pożegnań');
     channel.send(`Niestety ${member.user.tag} wyszedł z naszego serwera :(`);
 };
