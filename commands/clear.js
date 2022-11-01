8e97d1dba5985b2ccb24a8af887ba1508f6fdea2
hiayaf <98193137+hiayaf@users.noreply.github.com>
2022-11-01 17:36:04 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -9,12 +9,16 @@ module.exports = {
                 .setDescription('Ilość wiadomości do usunięcia (wartość od 1 do 100)')
                 .setRequired(true)),
     async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true });
-        const count = interaction.options.getInteger('ilość');
-        if (count < 101 && count > 0) {
-            await interaction.channel.bulkDelete(count, true).then(msg => { interaction.followUp({ content: `**Pomyślnie usunięto \`${msg.size}\` wiadomości.**`, ephemeral: true }); })
-        } else {
-            await interaction.followUp({ content: `**Nieprawidłowa wartość...** \`${count}\``, ephemeral: true });
+        if (interaction.member.permissions.has('ADMINISTRATOR')) {
+            await interaction.deferReply({ ephemeral: true });
+            const count = interaction.options.getInteger('ilość');
+            if (count < 101 && count > 0) {
+                await interaction.channel.bulkDelete(count, true).then(msg => {
+                    interaction.followUp({ content: `**Pomyślnie usunięto** \`${msg.size}\` **wiadomości.**`, ephemeral: true });
+                });
+            } else {
+                await interaction.followUp({ content: `**Nieprawidłowa wartość...** \`${count}\``, ephemeral: true });
+            };
         };
     }
 };
