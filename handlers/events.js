acee0d382189525c07a35dec9e07311390923bd9
hiayaf <98193137+hiayaf@users.noreply.github.com>
2022-11-14 23:04:57 +0100

    Update events.js

--- handlers/events.js
+++ handlers/events.js
@@ -1,12 +1,12 @@
-const { readdirSync } = require("fs");
+var fs = require('fs');
 module.exports = (client) => {
   const load = dirs => {
-    const events = readdirSync(`./events/${dirs}/`).filter(d => d.endsWith("js"));
+    const events = fs.readdirSync(`./events/${dirs}/`).filter(d => d.endsWith("js"));
     for (let file of events) {
-      let evt = require(`../events/${dirs}/${file}`);
-      let eName = file.split('.')[0];
-      client.on(eName, evt.bind(null, client));
-      console.log('[Eventy]' + ` Załadowano ` + eName);
+      let event = require(`../events/${dirs}/${file}`);
+      let eventName = file.split('.')[0];
+      client.on(eventName, event.bind(null, client));
+      console.log('[Eventy]' + ` Załadowano ` + eventName);
     }
   };
   ["client", "guild"].forEach((x) => load(x));
