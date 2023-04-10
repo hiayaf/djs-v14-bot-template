110764050d1a26862e28439856bba1e9904351bd
hiayaf <98193137+hiayaf@users.noreply.github.com>
2023-04-10 13:57:39 +0200

    Update apod_nasa.js

--- commands/apod_nasa.js
+++ commands/apod_nasa.js
@@ -1,30 +1,28 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const request = require('request');
+const axios = require('axios');
+const config = require('../config.json');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('apod')
         .setDescription('Nasa'),
     async execute(interaction) {
         await interaction.deferReply();
-        let url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
-        request(url, function (err, response, body) {
-            try {
-                body = JSON.parse(body);
-            } catch (error) {
-                return interaction.reply({ content: `Wystąpił błąd podczas uzyskiwania informacji od NASA`, ephemeral: true });
-            };
-            if (!err) {
-                const embed = new EmbedBuilder()
-                    .setImage(body.url)
-                    .setTitle(`Astronomy Picture of the Day`)
-                    .addFields(
-                        { name: `\u200B`, value: `**${body.title}**` },
-                        { name: '\u200B', value: `**${body.explanation}**` }
-                    )
-                    .setFooter({ text: body.date })
-                    .setAuthor({ name: body.copyright });
-                interaction.editReply({ embeds: [embed], ephemeral: false });
-            };
-        });
+        const url = "https://api.nasa.gov/planetary/apod?api_key=" + config.api_keys.NASA;
+        try {
+            const response = await axios.get(url);
+            const body = response.data;
+            const embed = new EmbedBuilder()
+                .setImage(body.url)
+                .setTitle(`Astronomy Picture of the Day`)
+                .addFields(
+                    { name: `Title`, value: `${body.title}` },
+                    { name: 'Explanation', value: `${body.explanation}` }
+                )
+                .setFooter(`${body.date}`)
+            await interaction.editReply({ embeds: [embed], ephemeral: false });
+        } catch (error) {
+            console.error(error);
+            await interaction.editReply({ content: `Wystąpił błąd podczas uzyskiwania informacji od NASA`, ephemeral: true });
+        }
     }
 };	
